b9e53c71c217f72e57a7e5061906245802bde24d
NinoMaverick <etengyemmy@gmail.com>
2024-02-27 15:47:16 +0100

    0x06-regex

--- 0x06-regular_expressions/0-simply_match_school.rb
+++ 0x06-regular_expressions/0-simply_match_school.rb
@@ -1,2 +1,20 @@
 #!/usr/bin/env ruby
-puts ARGV[0].scan(/School/).join
+# Ensure an argument is provided
+if ARGV.empty?
+    puts "Please provide an argument."
+    exit
+  end
+  
+  # Extract the argument
+  input_string = ARGV[0]
+  
+  # Define the regular expression pattern
+  pattern = /School/
+  
+  # Check if the input matches the pattern
+  if input_string.match?(pattern)
+    puts "The input '#{input_string}' contains the word 'School'."
+  else
+    puts "The input '#{input_string}' does not contain the word 'School'."
+  end
+  
