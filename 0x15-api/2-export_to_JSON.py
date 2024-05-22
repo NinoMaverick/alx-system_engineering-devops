1093f987080d3447ed29e8df3761ff1f992e55cb
NinoMaverick <etengyemmy@gmail.com>
2024-05-22 05:38:34 -0700

    api

--- 0x15-api/2-export_to_JSON.py
+++ 0x15-api/2-export_to_JSON.py
@@ -1,7 +1,5 @@
 #!/usr/bin/python3
-"""Records all tasks that are owned by this employee
-Format must be: { "USER_ID": [{"task": "TASK_TITLE", "completed": TASK_COMPLETED_STATUS, "username": "USERNAME"}, {"task": "TASK_TITLE", "completed": TASK_COMPLETED_STATUS, "username": "USERNAME"}, ... ]}
-File name must be: USER_ID.json"""
+"""Records all tasks that are owned by this employee"""
 
 import json
 import requests
